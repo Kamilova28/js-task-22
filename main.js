b52c91fe7aed92743f3a418023c5408d014e76b4
Kamilova28 <148527580+Kamilova28@users.noreply.github.com>
2024-01-17 00:27:27 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+// task 1
+
+//const ul = document.querySelector('ul');
+//const firstElement = ul.firstElementChild.textContent;
+//console.log(firstElement);
+
+
+// task 2
+
+// Remove the h1 element
+//document.querySelector("h1").remove();
+
+// Add a new list item with the text "TITAN RTX" to the end of the list
+//const listItem = document.createElement("li");
+//listItem.textContent = "TITAN RTX";
+//document.querySelector("ul").appendChild(listItem);
+
+// task 3
+
+//document.querySelector("h1").textContent = "GeForce 20 Series";
+
+//var elements = document.getElementsByClassName("primary");
+//for (var i = 0; i < elements.length; i++) {
+//elements[i].style.backgroundColor = "#ebebeb";
+
+
+// task 4
+
+//   var elements = document.getElementsByClassName("primary");
+
+//for(var i = 0; i < elements.length; i++) {
+  //  console.log(elements[i]);
+ // }
+
+ // task 5
+
+ //  var listItems = document.querySelectorAll("ul li");
+//listItems.forEach(function(item) {
+   // item.classList.add("item");
+  //});
+  
+ 
+  //var nonPrimaryItems = document.querySelectorAll("ul li:not(.primary)");
+  //nonPrimaryItems.forEach(function(item) {
+  //  item.classList.add("secondary");
+  //});
